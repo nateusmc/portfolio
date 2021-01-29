a45cc54431ae1ad7d686071ca4c046fa6de77b78
Nate Garrett <nate.garrett1992@gmail.com>
2021-01-29 15:47:55 -0600

    Added functionality to support multiple screenshots per project

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Project Selector
 let project1 = document.getElementsByClassName('project-1');
 let project2 = document.getElementsByClassName('project-2');
 let project3 = document.getElementsByClassName('project-3');
@@ -32,3 +33,123 @@ function showProject4() {
   project3[0].style.display = 'none';
   project4[0].style.display = 'flex';
 };
+
+// SlideShow Project 1
+var project1SlideIndex = 1;
+project1ShowSlides(project1SlideIndex);
+
+// Next/previous controls
+function project1PlusSlides(n) {
+  project1ShowSlides(project1SlideIndex += n);
+}
+
+// Thumbnail image controls
+function project1CurrentSlide(n) {
+  project1ShowSlides(project1SlideIndex = n);
+}
+
+function project1ShowSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("project1Slides");
+  var dots = document.getElementsByClassName("project1Dot");
+  if (n > slides.length) {project1SlideIndex = 1}
+  if (n < 1) {project1SlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[project1SlideIndex-1].style.display = "block";
+  dots[project1SlideIndex-1].className += " active";
+}
+
+// SlideShow Project 2
+var project2SlideIndex = 1;
+project2ShowSlides(project2SlideIndex);
+
+// Next/previous controls
+function project2PlusSlides(n) {
+  project2ShowSlides(project2SlideIndex += n);
+}
+
+// Thumbnail image controls
+function project2CurrentSlide(n) {
+  project2ShowSlides(project2SlideIndex = n);
+}
+
+function project2ShowSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("project2Slides");
+  var dots = document.getElementsByClassName("project2Dot");
+  if (n > slides.length) {project2SlideIndex = 1}
+  if (n < 1) {project2SlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[project2SlideIndex-1].style.display = "block";
+  dots[project2SlideIndex-1].className += " active";
+}
+
+// SlideShow Project 3
+var project3SlideIndex = 1;
+project3ShowSlides(project3SlideIndex);
+
+// Next/previous controls
+function project3PlusSlides(n) {
+  project3ShowSlides(project3SlideIndex += n);
+}
+
+// Thumbnail image controls
+function project3CurrentSlide(n) {
+  project3ShowSlides(project3SlideIndex = n);
+}
+
+function project3ShowSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("project3Slides");
+  var dots = document.getElementsByClassName("project3Dot");
+  if (n > slides.length) {project3SlideIndex = 1}
+  if (n < 1) {project3SlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[project3SlideIndex-1].style.display = "block";
+  dots[project3SlideIndex-1].className += " active";
+}
+
+// SlideShow Project 4
+var project4SlideIndex = 1;
+project4ShowSlides(project4SlideIndex);
+
+// Next/previous controls
+function project4PlusSlides(n) {
+  project4ShowSlides(project4SlideIndex += n);
+}
+
+// Thumbnail image controls
+function project4CurrentSlide(n) {
+  project4ShowSlides(project4SlideIndex = n);
+}
+
+function project4ShowSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("project4Slides");
+  var dots = document.getElementsByClassName("project4Dot");
+  if (n > slides.length) {project4SlideIndex = 1}
+  if (n < 1) {project4SlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[project4SlideIndex-1].style.display = "block";
+  dots[project4SlideIndex-1].className += " active";
+}
